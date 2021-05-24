602fb0d13601bac22863b6d9389cbe138dd84610
Srinivasan Parthasarathy <5309125+sriumcp@users.noreply.github.com>
2021-05-24 06:44:57 -0400

    updated crds
    Signed-off-by: Srinivasan Parthasarathy <5309125+sriumcp@users.noreply.github.com>

--- lib/metrics/collect.go
+++ lib/metrics/collect.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"sync"
+	"time"
 
 	"github.com/iter8-tools/etc3/api/v2alpha2"
 	"github.com/iter8-tools/handler/base"
+	"github.com/iter8-tools/handler/utils"
 )
 
 // Version contains header and url information needed to send requests to each version.
@@ -16,7 +19,7 @@ type Version struct {
 	// VersionInfo field of the experiment
 	Name string `json:"name" yaml:"name"`
 	// how many queries per second will be sent to this version; optional; default 8
-	QPS string `json:"qps,omitempty" yaml:"qps,omitempty"`
+	QPS *int32 `json:"qps,omitempty" yaml:"qps,omitempty"`
 	// HTTP headers to use in the query for this version; optional
 	Headers map[string]string `json:"headers,omitempty" yaml:"headers,omitempty"`
 	// URL to use for querying this version
@@ -26,13 +29,13 @@ type Version struct {
 // CollectInputs contain the inputs to the metrics collection task to be executed.
 type CollectInputs struct {
 	// how long to run the metrics collector; optional; default 5s
-	Time string `json:"time,omitempty" yaml:"time,omitempty"`
+	Time *string `json:"time,omitempty" yaml:"time,omitempty"`
 	// list of versions
 	Versions []Version `json:"versions" yaml:"versions"`
 	// URL of the JSON file to send during the query; optional
-	PayloadURL string `json:"payloadURL,omitempty" yaml:"payloadURL,omitempty"`
+	PayloadURL *string `json:"payloadURL,omitempty" yaml:"payloadURL,omitempty"`
 	// HTTP method which can be GET or POST; optional; default GET
-	Method string `json:"method,omitempty" yaml:"method,omitempty"`
+	Method *utils.HTTPMethod `json:"method,omitempty" yaml:"method,omitempty"`
 }
 
 // CollectTask enables collection of Iter8's built-in metrics.
@@ -43,10 +46,78 @@ type CollectTask struct {
 }
 
 // Run executes a CollectTask
+// figure out error handling every step of the way here ...
 func (t *CollectTask) Run(ctx context.Context) error {
 	log.Trace("collect task run started...")
-	var err error
-	return err
+	t.InitializeDefaults()
+	var wg sync.WaitGroup
+	fortioData := make(map[string]interface{})
+	// lock ensures thread safety while updating fortioData from go routines
+	var lock sync.Mutex
+	// if errors occur in one of the parallel threads, errCh is used to communicate them
+	errCh := make(chan error)
+	defer close(errCh)
+
+	for i := 0; i < len(t.With.Versions); i++ {
+		for j := range t.With.Versions {
+			// Increment the WaitGroup counter.
+			wg.Add(1)
+			// Launch a goroutine to fetch the Fortio data for this version.
+			go func(k int) {
+				// Decrement the counter when the goroutine completes.
+				defer wg.Done()
+				// Get Fortio data for version
+				data, err := t.fortioDataForVersion(k)
+				if err == nil {
+					// Update fortioData
+					lock.Lock()
+					fortioData[t.With.Versions[k].Name] = data
+					lock.Unlock()
+				} else {
+					errCh <- err
+				}
+			}(j)
+		}
+	}
+
+	// See https://stackoverflow.com/questions/32840687/timeout-for-waitgroup-wait
+	// Compute timeout as duration of fortio requests + 30s
+	dur, err := time.ParseDuration(*t.With.Time)
+	if err != nil {
+		return err
+	}
+	if err = utils.WaitTimeoutOrError(&wg, dur+30*time.Second, errCh); err != nil {
+		log.Error(err)
+		return err
+	} else {
+		log.Trace("Wait group finished normally")
+	}
+
+	return nil
+}
+
+// fortioDataForVersion collects fortio data for a given version
+func (t *CollectTask) fortioDataForVersion(j int) (map[string]interface{}, error) {
+	return make(map[string]interface{}), nil
+}
+
+// InitializeDefaults sets the default values for optional fields that are empty
+func (t *CollectTask) InitializeDefaults() {
+	if t.With.Time == nil {
+		t.With.Time = utils.StringPointer("5s")
+	}
+	if t.With.Method == nil {
+		if t.With.PayloadURL == nil {
+			t.With.Method = utils.HTTPMethodPointer(utils.GET)
+		} else {
+			t.With.Method = utils.HTTPMethodPointer(utils.POST)
+		}
+	}
+	for i := 0; i < len(t.With.Versions); i++ {
+		if t.With.Versions[i].QPS == nil {
+			t.With.Versions[i].QPS = utils.Int32Pointer(8)
+		}
+	}
 }
 
 // MakeCollect converts a collect task spec into a CollectTask
--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,10 @@
 package utils
 
 import (
+	"errors"
+	"sync"
+	"time"
+
 	iter8utils "github.com/iter8-tools/etc3/util"
 	"github.com/sirupsen/logrus"
 )
@@ -29,3 +33,44 @@ func GetLogger() *logrus.Logger {
 
 // CompletePath determines complete path of a file
 var CompletePath func(prefix string, suffix string) string = iter8utils.CompletePath
+
+// Int32Pointer takes an int32 as input, creates a new variable with the input value, and returns a pointer to the variable
+func Int32Pointer(i int32) *int32 {
+	return &i
+}
+
+// StringPointer takes a string as input, creates a new variable with the input value, and returns a pointer to the variable
+func StringPointer(s string) *string {
+	return &s
+}
+
+type HTTPMethod string
+
+const (
+	GET  HTTPMethod = "GET"
+	POST            = "POST"
+)
+
+// HTTPMethodPointer takes an HTTPMethod as input, creates a new variable with the input value, and returns a pointer to the variable
+func HTTPMethodPointer(h HTTPMethod) *HTTPMethod {
+	return &h
+}
+
+// WaitTimeout waits for the waitgroup for the specified max timeout.
+// Returns true if waiting timed out.
+// See https://stackoverflow.com/questions/32840687/timeout-for-waitgroup-wait
+func WaitTimeoutOrError(wg *sync.WaitGroup, timeout time.Duration, errCh chan error) error {
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		wg.Wait()
+	}()
+	select {
+	case <-c:
+		return nil // completed normally
+	case <-time.After(timeout):
+		return errors.New("Timedout waiting for fortio data collection") // timed out
+	case err := <-errCh:
+		return err
+	}
+}
